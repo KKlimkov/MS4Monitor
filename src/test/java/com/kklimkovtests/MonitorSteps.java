0acc65d7a78c1f5b993108d9dc8cdbb612b74b42
KKlimkov <k.klimkov@list.ru>
2021-09-21 16:11:59 +0300

    Add Steps and Allure fixes

---
+++ src/test/java/com/kklimkovtests/MonitorSteps.java
@@ -0,0 +1,130 @@
+package com.kklimkovtests;
+
+import io.appium.java_client.windows.WindowsDriver;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+import java.util.List;
+import io.qameta.allure.Step;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MonitorSteps {
+
+    public static WindowsDriver driver2 = null;
+
+    @Step("Запуск драйвера root")
+    public static void LaunchRoot() throws InterruptedException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("platformName", "Windows");
+        cap.setCapability("deviceName", "WindowsPC");
+        cap.setCapability("app", "Root");
+        cap.setCapability("ms:waitForAppLaunch", "10");
+        try {
+            driver2 = new WindowsDriver(new URL("http://127.0.0.1:4723"), cap);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Step("Запуск Monitor из tray")
+    public static void LaunchTray() throws InterruptedException {
+        Actions actionProvider = new Actions(driver2);
+        driver2.findElementByName("Шеврон уведомления").click();
+        Thread.sleep(500);
+        assertTrue(driver2.findElements(By.name("MasterSCADA 4D RT 1_2")).size() > 0);
+        WebElement MonitorTest1 = driver2.findElementByName("MasterSCADA 4D RT 1_2");
+        actionProvider.doubleClick(MonitorTest1).perform();
+        Thread.sleep(500);
+    }
+
+    @Step("Остановить все процессы")
+    public static void StopAllProcess() throws InterruptedException {
+        List<WebElement> elements = driver2.findElements(By.name("ВСЕ"));
+        System.out.println(elements);
+        elements.get(1).click();
+        Thread.sleep(5000);
+    }
+
+    @Step("Запустить все процессы все процессы")
+    public static void StartAllProcess() throws InterruptedException {
+        List<WebElement> elements = driver2.findElements(By.name("ВСЕ"));
+        System.out.println(elements);
+        elements.get(0).click();
+        Thread.sleep(5000);
+    }
+
+    @Step("Проверка на существование процесса")
+    public static void GetProcessExistence(String ProcessName,Boolean Ex) throws IOException {
+        String err;
+        String line = "";
+        System.out.println("Start Search Process:  " + ProcessName);
+        Process p1 = Runtime.getRuntime().exec("tasklist /FI \"IMAGENAME eq " + ProcessName + ".exe\"");
+        InputStream stderr = p1.getErrorStream();
+        InputStream stdout = p1.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout, "Cp866"));
+        BufferedReader error = new BufferedReader(new InputStreamReader(stderr, "Cp866"));
+        String Out = null;
+        while ((err = error.readLine()) != null) {
+            System.out.println(err);
+        }
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            if (Ex) {
+                assertTrue(!line.contains("Задачи, отвечающие заданным критериям, отсутствуют"));
+            } else
+            {assertTrue(line.contains("Задачи, отвечающие заданным критериям, отсутствуют"));};
+        }
+    }
+
+    @Step("Переоткрыть окно монитора")
+    public static void ReopenWindowMonitor() throws InterruptedException {
+        driver2.findElementByAccessibilityId("ReopenAmongButtons").click();
+        Thread.sleep(1000);
+    }
+
+    @Step("Клик на элемент окна монитора")
+    public static void ClickToMonitorElement(String NameElement, Integer Waiting) throws InterruptedException {
+        driver2.findElementByName(NameElement).click();
+        Thread.sleep(Waiting);
+    }
+
+    @Step("Выбор проекта для импорта")
+    public static void ChoiceImportProject() throws InterruptedException {
+        driver2.findElementByClassName("UIProperty").click();
+        Thread.sleep(1000);
+        driver2.findElementByName("Открыть").click();
+        Thread.sleep(1000);
+    }
+
+    @Step("Проверка на существование элементов")
+    public static void CheckElementExistence(String Whatfind, Integer AccSize, String AssertMessage) throws InterruptedException {
+        Boolean DelNotWork = driver2.findElementsByName(Whatfind).size()>AccSize;
+        System.out.println(driver2.findElementsByName(Whatfind).size());
+        assertFalse(DelNotWork, AssertMessage);
+        Thread.sleep(2000);
+        /*if (DelNotWork) {
+            System.out.println(driver2.findElementsByName("Удалить").size());
+            driver2.findElementByName(" Отмена ").click();
+            Thread.sleep(2000);
+            StartAllProcess();
+            driver2.findElementByAccessibilityId("ReopenAmongButtons").click();
+            Thread.sleep(2000);
+            List<WebElement> elements2 = driver2.findElements(By.name("ВСЕ"));
+            StartAllProcess();
+            Thread.sleep(3000);
+            assertFalse(DelNotWork, "Кнопка Удалить не работает. Выход нажатием Отмена");
+        };
+        */
+    }
+
+}
---
+++ src/test/java/com/kklimkovtests/TestMonitor.java
@@ -0,0 +1,104 @@
+package com.kklimkovtests;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.*;
+import java.io.IOException;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+@Owner("KKlimkov")
+@Layer("monitor")
+@Feature("MS4D RT Monitor")
+
+@TestMethodOrder(OrderAnnotation.class)
+public class TestMonitor {
+
+
+    @DisplayName("Запуск монитора из панели задач")
+    @Test
+    @Story("Launch Monitor")
+    @Tags({@Tag("Monitor"),@Tag("Process")})
+    @Order(1)
+    public void Launch() throws InterruptedException {
+        MonitorSteps.LaunchRoot();
+        MonitorSteps.LaunchTray();
+    }
+
+    @DisplayName("Проверка на существование процессов")
+    @Test
+    @Story("CheckProcess")
+    @Tags({@Tag("Monitor"),@Tag("Process")})
+    @Order(2)
+    public void LaunchMPLCALL() throws InterruptedException, IOException {
+        MonitorSteps.StopAllProcess();
+        MonitorSteps.GetProcessExistence("mplc", false);
+        MonitorSteps.GetProcessExistence("nginx", false);
+        MonitorSteps.GetProcessExistence("node_ms4d", false);
+        MonitorSteps.StartAllProcess();
+        MonitorSteps.GetProcessExistence("mplc", true);
+        MonitorSteps.GetProcessExistence("nginx", true);
+        MonitorSteps.GetProcessExistence("node_ms4d", true);
+        MonitorSteps.ReopenWindowMonitor();
+    }
+
+    @DisplayName("Запуск отдельных процессов")
+    @Test
+    @Story("CheckSeparateProcess")
+    @Tags({@Tag("Monitor"),@Tag("Process")})
+    @Order(3)
+    public void LaunchOnlymplc() throws InterruptedException, IOException {
+        MonitorSteps.ClickToMonitorElement("mplc",500);
+        MonitorSteps.ClickToMonitorElement("Остановить процесс",5000);
+        MonitorSteps.GetProcessExistence("mplc", false);
+        MonitorSteps.GetProcessExistence("nginx", true);
+        MonitorSteps.GetProcessExistence("node_ms4d", true);
+        MonitorSteps.ClickToMonitorElement("Запустить процесс",5000);
+        MonitorSteps.GetProcessExistence("mplc", true);
+        MonitorSteps.GetProcessExistence("nginx", true);
+        MonitorSteps.GetProcessExistence("node_ms4d", true);
+    }
+
+    @DisplayName("Импорт проектов")
+    @Test
+    @Story("ImportProject")
+    @Tags({@Tag("Monitor"),@Tag("Import")})
+    @Order(4)
+    public void ImportProject() throws InterruptedException, IOException {
+        MonitorSteps.StopAllProcess();
+        MonitorSteps.ClickToMonitorElement("Импорт проекта",1000);
+        MonitorSteps.ChoiceImportProject();
+        MonitorSteps.ClickToMonitorElement("Импортировать",1000);
+        MonitorSteps.StartAllProcess();
+        MonitorSteps.ReopenWindowMonitor();
+        MonitorSteps.ClickToMonitorElement("Ventilation (АРМ 1)",1000);
+    }
+
+    @DisplayName("Запуск клиента")
+    @Test
+    @Story("LaunchClient")
+    @Tags({@Tag("Monitor"),@Tag("Embedded Client")})
+    @Order(5)
+    public void StartClient() throws InterruptedException, IOException {
+        MonitorSteps.ClickToMonitorElement("Запустить клиент",7000);
+        MonitorSteps.GetProcessExistence("MasterSCADA4DClient", true);
+        MonitorSteps.ClickToMonitorElement("Закрыть",2000);
+        MonitorSteps.GetProcessExistence("MasterSCADA4DClient", false);
+    }
+
+    @DisplayName("Удаление проекта")
+    @Test
+    @Story("LaunchClient")
+    @Tags({@Tag("Monitor"),@Tag("DelProject")})
+    @Order(6)
+    public void DelProject() throws InterruptedException, IOException {
+        MonitorSteps.StopAllProcess();
+        MonitorSteps.ClickToMonitorElement("Удалить проект",1000);
+        MonitorSteps.ClickToMonitorElement("Удалить проект",3000);
+        MonitorSteps.ClickToMonitorElement("Удалить",5000);
+        MonitorSteps.CheckElementExistence("Другие файлы",0,"Нет кнопки Другие файлы");
+        MonitorSteps.StartAllProcess();
+        //MonitorSteps.ReopenWindowMonitor();
+        MonitorSteps.CheckElementExistence("Ventilation (АРМ 1)",0, "Удаление не сработало");
+    }
+
+}
